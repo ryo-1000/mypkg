6761eb67081e71abb9cb5c2273b0313328eb77dc
ryo <s21c1021um@s.chibakoudai.jp>
2022-12-08 12:39:45 +0900

    test

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,19 +3,18 @@ from rclpy.node import Node
 from person_msgs.msg import Int16
 
 class Talker():
-    def __init__(self):
+    def __init__(self, node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
+        node.create_timer(0.5, self.cb)
+
+def cd(self):
+    msg = Int16()
+    msg.data = self.n
+    self.pub.publish(msg)
+    self.n += 1
 
 rclpy.init()
 node = Node("talker")
-talker = Talker()
-
-def cb():
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
-
-node.create_timer(0.5, cb)
+talker = Talker(node)
 rclpy.spin(node)
