88b3a05c4874750962df3d936c561b605168554f
ryo <s21c1021um@s.chibakoudai.jp>
2022-12-31 21:49:23 +0900

    test

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Int16
+from std_msgs.msg import Int16
 
 def cb(msg):
     global node
--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts':[
-              'talker = mypkg.talker:main',
-              'listener = mypkg.listener:main',
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
         ],
     },
 )
