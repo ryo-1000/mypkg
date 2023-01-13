f1b415206db75dec7335c1f7477361819feaf317
ryo <s21c1021um@s.chibakoudai.jp>
2023-01-13 16:49:46 +0900

    test

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2022 Ryo Ebisawa
+# SPDX-Licencse-Identifier: BSD-3-Clause
+
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
